f6499e378e96e480c0522eb9361dad43c1839d20
koji-noguchi <info@interight.co.jp>
2024-07-23 17:49:41 +0900

    add html page

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 # syntax=docker/dockerfile:1
 FROM alpine:node:20.15.1-alpine3.20
-
+WORKDIR /app
+COPY . /app/
+RUN npm install
+COPY . .
+EXPOSE 3000
+CMD ["npm", "devstart"]
 
---
+++ statics/js/index.js
@@ -0,0 +1,23 @@
+function printName(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`)
+
+  return `${firstName} ${lastName}`
+}
+
+function printNTimes(n, value) {
+  debugger
+  for(let i = 0; i < n; i++) {
+    console.log(value)
+  }
+}
+
+document.addEventListener("click", () => {
+  console.log("Clicked")
+})
+
+setTimeout(() => {
+  console.log("Time")
+}, 1000)
+
+printName("Kyle", "Cook")
+printNTimes(5, "Hi")
